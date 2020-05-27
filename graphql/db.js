2a3f3cc349e72e44658266108a0db6732549be25
pathas <pathas@naver.com>
2020-05-27 12:18:01 +0900

    Improve API

--- graphql/db.js
+++ graphql/db.js
@@ -1,15 +1,45 @@
-const API_URL = "https://yts.am/api/v2/list_movies.json?";
-import fetch from "node-fetch";
+import axios from "axios";
+const BASE_URL = "https://yts.am/api/v2/";
+const MOVIES_LIST_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS = `${BASE_URL}movie_suggestions.json`;
 
-export const getMovies = (limit, rating) => {
-  let REQUEST_URL = API_URL;
-  if (limit > 0) {
-    REQUEST_URL += `limit=${limit}`;
-  }
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
-  }
-  return fetch(REQUEST_URL)
-    .then((res) => res.json())
-    .then((json) => json.data.movies);
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIES_LIST_URL, {
+    params: {
+      limit,
+      rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAIL_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies, movie_count },
+    },
+  } = await axios(MOVIE_SUGGESTIONS, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return { movie_count, movies };
 };
